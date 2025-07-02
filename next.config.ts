4e319a4c80f7ec9ad02b8dfa19349e9684170426
kali <kali@kali.kali>
2025-07-02 05:37:23 -0400

    Url changed for backedn

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "nutvahealth.uz",
+        hostname: "api.nutvahealth.uz",
         port: "",
         pathname: "/uploads/**",
       },
