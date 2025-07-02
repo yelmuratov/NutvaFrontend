39a905a705f6976d8f7deaeba873908224e3c13a
kali <kali@kali.kali>
2025-07-02 05:30:20 -0400

    Url changed for backedn

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,37 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "api.nutvahealth.uz",
-				port: "",
-				pathname: "/uploads/**",
-			},
-		],
-		unoptimized: true,
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.nutvahealth.uz",
+        port: "",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "nutvahealth.uz",
+        port: "",
+        pathname: "/uploads/**",
+      },
+    ],
+    unoptimized: true,
+  },
 
-	async rewrites() {
-		return [
-			{
-				source: "/api/:path((?!auth).*)",
-				destination: "https://api.nutvahealth.uz/api/:path*",
-			},
-		];
-	},
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path((?!auth).*)",
+        destination: "https://api.nutvahealth.uz/api/:path*",
+      },
+    ];
+  },
 
-	env: {
-		NEXT_PUBLIC_BASE_URL: "https://api.nutvahealth.uz/api",
-		NEXT_PUBLIC_API_URL: "https://api.nutvahealth.uz/api",
-	},
+  env: {
+    NEXT_PUBLIC_BASE_URL: "https://api.nutvahealth.uz/api",
+    NEXT_PUBLIC_API_URL: "https://api.nutvahealth.uz/api",
+  },
 };
 
 export default nextConfig;
