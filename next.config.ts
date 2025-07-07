e9e42c2693748a8ee11e95ce7d05824b40305607
yelmuratov <yelimuratovsalimbay@gmail.com>
2025-07-07 21:13:55 +0500

    Bloge detail page error

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "demo.nutva.uz",
-				port: "",
-				pathname: "/uploads/**",
-			},
-		],
-		unoptimized: true,
-	},
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "demo.nutva.uz",
+        port: "",
+        pathname: "/uploads/**",
+      },
+    ],
+    unoptimized: true,
+  },
 
-	async rewrites() {
-		return [
-			{
-				source: "/api/:path*",
-				destination: "https://demo.nutva.uz/api/:path*",
-			},
-		];
-	},
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://demo.nutva.uz/api/:path*",
+      },
+    ];
+  },
 
-	env: {
-		NEXT_PUBLIC_BASE_URL: "https://demo.nutva.uz/api",
-		NEXT_PUBLIC_API_URL: "https://demo.nutva.uz/api",
-	},
+  env: {
+    NEXT_PUBLIC_BASE_URL: "https://demo.nutva.uz/api",
+    NEXT_PUBLIC_API_URL: "https://demo.nutva.uz/api",
+  },
 };
 
 export default nextConfig;
