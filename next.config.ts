39a64987a35738e3ebc7347adf87d5e238893c8d
yelmuratov <yelimuratovsalimbay@gmail.com>
2025-07-07 21:37:23 +0500

    Url changed"

--- next.config.ts
+++ next.config.ts
@@ -17,15 +17,17 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
+        // Route API calls from the browser to the external domain
         source: "/api/:path*",
-        destination: "https://demo.nutva.uz/api/:path*",
+        destination: "https://demo.nutva.uz/api/:path*", // Keep this for client-side requests
       },
     ];
   },
 
   env: {
-    NEXT_PUBLIC_BASE_URL: "https://demo.nutva.uz/api",
-    NEXT_PUBLIC_API_URL: "https://demo.nutva.uz/api",
+    // Used in server-side functions like generateMetadata, getServerSideProps, etc.
+    NEXT_PUBLIC_BASE_URL: "http://localhost:5000/api",
+    NEXT_PUBLIC_API_URL: "http://localhost:5000/api",
   },
 };
 
